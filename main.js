36de065b273d3a12643eed96bc66d66f72cb9ec4
Cristiano <valentisenior@gmail.com>
2020-04-09 19:13:52 +0200

    Primo passo jquery

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ $(document).ready(function(){
   send.click(
     function () {
       console.log(input.val());
-      div.append('<div class = "msginviato"><span>'+input.val()+'</span></div>');
+      div.append('<div class = "msginviato incomune"><span>'+input.val()+'</span><span><i class="fa fa-chevron-down"></i></span></div>');
       input.val("");
       //dopo un secondo ok
       // deve apparire un nuovo msg con un testo sempre uguale (statico) ok
       setTimeout(
         function(){
-        div.append('<div class = "msgricevuto"><span>ok</span></div>');
+        div.append('<div class = "msgricevuto incomune"><span>ok</span><span><i class="fa fa-chevron-down"></i></span></div>');
       },1000);
 
     }
@@ -28,6 +28,7 @@ $(document).ready(function(){
         console.log(testoInserito);
       // selezionare tutti i blocchi di contatto e ciclare tra di essi (each())
        var contatti = $('.primoContatto');
+
        contatti.each(
          function(){
            //salvo in una var il valore del testo del nome nel contatto (stringa2)
@@ -45,13 +46,28 @@ $(document).ready(function(){
        )
       }
     )
+    // Cancella messaggio:
+    //cliccando sul messaggio appare un menu a tendina
+  var freccina = $(".incomune span i");
+  var cancella = $(".cancella");
+  freccina.click(
+    function(){
+      $(this).parent("div + .cancella").toggle();
+    }
 
-
-
-
-
-
-
-
-
+  )
+  //   che permette di cancellare il messaggio selezionato
 });
+
+//Click sul contatto mostra la conversazione del contatto cliccato
+// click sul contatto che ha data-attr che corrisponde a stesso data-attr in chat
+// salvo il valore dell’attr e lo usso per dire quale chat è attiva
+// è possibile inserire nuovi messaggi per ogni conversazione [attiva]
+
+
+// son riuascito ad agganciarte l’evento sul “delete” potrò dirgli una roba tipo this.padre.cancella();
+// $(‘.right-messages’).on(“click”, “.message”,
+// function () {
+// alert(“hai cliccato su di me!“);
+// $(this).hide();
+// }
